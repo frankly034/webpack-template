0244bcbec9af75b912368f3e16c11b7bfa1683ad
frankly034 <franklynugege@gmail.com>
2019-08-17 21:49:15 +0100

    chore(setup): setup webpack
    - initialize node
    - add and setup babel dependencies
    - add and setup webpack dependencies
    - add and setup eslint with react
    - add start scripts
    - add app component
    
    [Finsihes]

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = () => <h1>Hello World.</h1>
+
+render(<App />, document.querySelector('#root'));
+
